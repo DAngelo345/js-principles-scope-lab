ea96be211768a541e67fcfa24f1a435e0ee0792c
DAngelo Thomas <dangelo1thomas@gmail.com>
2021-08-02 00:46:13 -0400

    commit tree 05d5a3a347d31452d4b0e41840f05c1e5d723f56

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+
+    customerName = customerName.toUpperCase();
+    
+}
+
+function setBestCustomer() {
+
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+
+    bestCustomer = 'maybe bob';
+}
+
+// function leastFavoriteCustomer() {
+
+// }
+
+const leastFavoriteCustomer = 'newv';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'changed newv';
+
+}
